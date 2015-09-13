d977ea6b174ee157bd459d8a711e93316c6d776a
Ken Hopwood <ken.hopwood@gmail.com>
2015-09-13 09:58:54 -0700

    Creation

---
+++ Server/getWeatherForecast.py
@@ -0,0 +1,28 @@
+__author__ = 'kenhopwood'
+# Get Weather Forecast
+# 13-SEP-2015
+# K.Hopwood
+
+import socket
+import urllib2
+
+def getWeatherForecast(stationid):
+
+    # timeout in seconds
+    timeout = 10
+    socket.setdefaulttimeout(timeout)
+#TODO: get actual APPID
+    req = urllib2.Request('api.openweathermap.org/data/2.5/weather?forecast?id='+str(stationid)+ '&APPID=1111111111')
+    try:
+        response = urllib2.urlopen(req)
+    except urllib2.URLError as e:
+        if hasattr(e, 'reason'):
+            print 'We failed to reach a server.'
+            print 'Reason: ', e.reason
+        elif hasattr(e, 'code'):
+            print 'The server couldn\'t fulfill the request.'
+            print 'Error code: ', e.code
+    else:
+        # everything is fine
+        return response
+
